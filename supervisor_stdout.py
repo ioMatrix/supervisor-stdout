7be8c1c5517ca43b3e487b034c37b70992e17da0
ioMatrix <iomatrix@gmail.com>
2020-01-23 11:37:28 -0700

    Fix bug in output syntax

--- supervisor_stdout.py
+++ supervisor_stdout.py
@@ -21,7 +21,7 @@ def event_handler(event, response):
     line, data = response.split('\n', 1)
     headers = dict([ x.split(':') for x in line.split() ])
     for newline in data.splitlines():
-        print '[%s] [%s] %s'%(headers['processname'], headers['channel'], newline)
+        print( '[{0}] [{1}] {2}'.format(headers['processname'], headers['channel'], newline) )
 
 if __name__ == '__main__':
     main()
